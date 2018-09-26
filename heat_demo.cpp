d2b7945851c1ea7c42ecc54af32dcad7e7b89f81
Matthew Crankshaw <mhcrankshaw2@gmail.com>
2018-09-27 10:07:24 +1200

    started working on the multithreading of the program

--- heat_demo.cpp
+++ heat_demo.cpp
@@ -2,6 +2,8 @@
 Start up demo program for 159735 Assignment 3 Semester 1 2013
 ******************************************************************************/
 #include <iostream>
+#include <omp.h>
+#include <time.h>
 
 #include "arrayff.hxx"
 #include "draw.hxx"
@@ -18,7 +20,7 @@ int main(int argc, char* argv[])
 	Array<float, 2> h(npixy, npixy), g(npixy, npixx);
 
 	const int nrequired = npixx * npixy;
-	const int ITMAX = 10000000;
+	const int ITMAX = 100000;
 
 	int iter = 0;
 	int nconverged = 0;
@@ -26,27 +28,62 @@ int main(int argc, char* argv[])
 	fix_boundaries2(h);
 	dump_array<float, 2>(h, "plate0.fit");
 
+	omp_set_num_threads(2);
+
+	clock_t begin = clock();
+
+	int sizePerThread, startpoint, endpoint;
+
 	do {
-    	for (int y = 1; y < npixy-1; ++y) {
-	    	for (int x = 1; x < npixx-1; ++x) {
-				g(y, x) = 0.25 * (h(y, x-1) + h(y, x+1) + h(y-1, x) + h(y+1,x));
-	      	}
+
+		#pragma omp parallel 
+		{
+			sizePerThread = npix / omp_get_num_threads();
+			startpoint = omp_get_thread_num() * sizePerThread;
+			endpoint = startpoint + sizePerThread;
+
+	    	for (int y = startpoint; y < endpoint; ++y) {
+		    	for (int x = 1; x < npixx-1; ++x) {
+		    		if(y == 0) continue; 
+		    		if(y == npix - 1) continue;
+		    		if((y == startpoint) || (y == endpoint-1)){ 
+		    			#pragma omp critical
+	    				g(y, x) = 0.25 * (h(y, x-1) + h(y, x+1) + h(y-1, x) + h(y+1,x));
+	    			}else{
+						g(y, x) = 0.25 * (h(y, x-1) + h(y, x+1) + h(y-1, x) + h(y+1,x));
+					}
+		      	}
+		    }
 	    }
 
 	    fix_boundaries2(g);
 
 	    nconverged = 0;
-	    for (int y = 0; y < npixy; ++y) {
-	    	for (int x = 0; x < npixx; ++x) {
-				float dhg = std::fabs(g(y, x) - h(y, x));
-				if (dhg < tol) ++nconverged;
-				h(y, x) = g(y, x);
-	      	}
-	    }
-	    ++iter;
+	    #pragma omp parallel 
+		{
+			sizePerThread = npix / omp_get_num_threads();
+			startpoint = omp_get_thread_num() * sizePerThread;
+			endpoint = startpoint + sizePerThread;
 
-	  }while (nconverged < nrequired && iter < ITMAX);
+		    for (int y = startpoint; y < endpoint; ++y) {
+		    	for (int x = 0; x < npixx; ++x) {
+					float dhg = std::fabs(g(y, x) - h(y, x));
+					if (dhg < tol){
+						#pragma omp critical
+						++nconverged;
+					}
+					h(y, x) = g(y, x);
+		      	}
+		    }
+		}
+		std::cout << nconverged << std::endl;
+	    ++iter;
+	}while (nconverged < nrequired && iter < ITMAX);
+
+  	clock_t end = clock();
 
 	dump_array<float, 2>(h, "plate1.fit");
 	std::cout << "Required " << iter << " iterations" << std::endl;
+
+	std::cout << "total time: " << (double)(end - begin) / CLOCKS_PER_SEC << std::endl;
 }
